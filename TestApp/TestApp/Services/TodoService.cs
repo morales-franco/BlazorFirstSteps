03e9c03a34c60d2264913da5dbd29ca5a0180cab
Morales Franco <franco.e.morales@gmail.com>
2020-10-17 14:36:17 -0300

    Add project

---
+++ TestApp/TestApp/Services/TodoService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestApp.ViewModels;
+
+namespace TestApp.Services
+{
+    public class TodoService
+    {
+        private static IList<TodoVM> _todos = new List<TodoVM>() {
+            new TodoVM
+            {
+                Title = "A",
+                IsDone = true
+            },
+            new TodoVM
+            {
+                Title = "B",
+                IsDone = false
+            }
+        };
+
+
+        public Task<IList<TodoVM>> GetTodosAync()
+        {
+            var rng = new Random();
+            return Task.FromResult(_todos);
+        }
+    }
+}
---
+++ TestApp/TestApp/ViewModels/TodoVM.cs
@@ -0,0 +1,8 @@
+﻿namespace TestApp.ViewModels
+{
+    public class TodoVM
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
